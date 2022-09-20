dc3913f7d45d82d502bb596887811b7a612b870b
Rainy <goncalvesfrancisco2013@gmail.com>
2022-09-20 13:42:27 +0100

    Revert "second"
    This reverts commit 500ccad150bc2ec128c80fef02309f0aee5d79e8.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import yaml
-import json
+
+
 def printFibonnaci(maxNbrs):
     sec=0
     last=1
@@ -15,8 +15,5 @@ def printFibonnaci(maxNbrs):
 nbrs2print=input("Choose how many numbers of the fibonacci sequence you wanna print: ")
 printFibonnaci(int(nbrs2print))
 
-with open("data.yml","r+") as file:
-    user=yaml.safe_load(file)
-    user['user']['format']="JSON"
-    with open("data.json","w") as file:
-        json.dump(user,file)
+
+
