b2706f58e2df46b599918e4f4ded6535f57da058
Rainy <goncalvesfrancisco2013@gmail.com>
2022-09-20 16:01:28 +0100

    testing

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
 import json
-def printFibonnaci(maxNbrs):
-    listerinos=[]
-    sec=0
-    last=1
-    for i in range(0,maxNbrs):
-        if(i<2):
-            listerinos.append(i)
-            continue
-        temp=sec+last
-        last=sec
-        sec=temp
-        listerinos.append(temp)
-    return listerinos
+import unittest
+class CustomParser:
+    
+    def parseJson(self):
+        with open("data.json","r") as jsonfile:
+            data=json.load(jsonfile)
+            name=data['user']['name']
+            return name
 
-nbrs2print=input("Choose how many numbers of the fibonacci sequence you wanna print: ")
-with open("data.json","w") as file:
-    json.dump("{fibosequence:"+str(printFibonnaci(int(nbrs2print)))+"}",file)
-
+class TestParse(unittest.TestCase):
+    def test_parse_user_name(self):
+        cp=CustomParser()
+        expectedName="Rainy"
+        parsedName=cp.parseJson()
+        self.assertEqual(expectedName,parsedName)
