d790ec095bb66eca6f837d9fae1022b319ca9644
Rainy <goncalvesfrancisco2013@gmail.com>
2022-09-20 14:05:30 +0100

    small changes

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 
 
 def printFibonnaci(maxNbrs):
+    listerinos=[]
     sec=0
     last=1
     for i in range(0,maxNbrs):
         if(i<2):
-            print(i)
+            listerinos.append(i)
             continue
         temp=sec+last
         last=sec
         sec=temp
-        print(temp)
+        listerinos.append(temp)
+    return listerinos
 
 nbrs2print=input("Choose how many numbers of the fibonacci sequence you wanna print: ")
 printFibonnaci(int(nbrs2print))
-
-
 
